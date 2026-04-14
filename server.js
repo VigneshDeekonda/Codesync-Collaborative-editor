226029ca4aa31829178fb87cd35fb68756027b9a
VigneshDeekonda <vignesh.deekonda5002@gmail.com>
2026-04-15 02:33:37 +0530

    fix: socket.io cors wildcard *

--- server.js
+++ server.js
@@ -29,15 +29,9 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: "*",
     methods: ["GET", "POST"],
-    credentials: true,
+    credentials: true
   },
 });
 
